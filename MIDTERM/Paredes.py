944230b0fcd7690337728583030e2a9b04223c71
unknown <86631463+Arjeclyde@users.noreply.github.com>
2024-10-03 02:16:03 +0800

    Create Paredes.py

---
+++ MIDTERM/Paredes.py
@@ -0,0 +1,16 @@
+number_1 = float(input("Enter a Number: "))
+number_2 = float(input("Enter a Second Number: "))
+Calculator = input("Add, Subtract, Multiply or Divide:")
+
+if Calculator == "Add":
+    print(number_1 + number_2)
+elif Calculator == "Subtract":
+    print(number_1 - number_2)
+elif Calculator == "Multiply":
+    print(number_1 * number_2)
+else:
+    print(number_1 / number_2)
+
+
+# for Number in range (2,101,2):
+#     print(Number)
