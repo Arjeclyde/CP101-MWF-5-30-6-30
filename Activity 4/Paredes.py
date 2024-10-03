b4a595aa92adcefc706ce129ead00d71f52de672
unknown <86631463+Arjeclyde@users.noreply.github.com>
2024-10-03 22:26:57 +0800

    Create Paredes.py

---
+++ Activity 4/Paredes.py
@@ -0,0 +1,11 @@
+# sampleText1 = "My name is {}, I love {}, I love playing {},"
+# sampleText1a = sampleText1.format("Arje clyde", "Myself", "Video Games")
+# print(sampleText1a)
+
+# sampleText2 = "My name is {1}, I love {2},I love playing {0}, "
+# sampleText2a = sampleText2.format("carrots","Arje clyde", "Myself", "Video Games","Banana")
+# print(sampleText2a)
+
+# sampleText3 = "My name is {name}, I love {Me}, I love playing {play},"
+# sampleText3a = sampleText3.format(Me="Myself", play="Video Games", name="Arje clyde")
+# print(sampleText3a)
