1caaedaeb3c3aaee7b8e29737ee73c43ce45e65b
unknown <86631463+Arjeclyde@users.noreply.github.com>
2024-11-03 15:52:01 +0800

    Paredes.py

--- SEMI FINAL EXAM/Paredes.py
+++ SEMI FINAL EXAM/Paredes.py
@@ -1,19 +1,19 @@
 record = {}
 while True:
     print(record)
-    choice = input("Choose an Option \n 1. Add Data \n 2. Delete Data \n 3. End \n")
-    if choice == '1':
+    choice = input("Choose an Option \n A. Add Data \n B. Delete Data \n C. End \n")
+    if choice == 'A':
         key = input("Enter Key: ")
         value = input("Enter a Value: ")
         record[key] = value
         print("Data Added Successfully. ")
-    elif choice == '2':
+    elif choice == 'B':
         key = input ("Enter key: ")
         if key in record:
             del record[key]
             print("Data Deleted Successfully. ")
         else:
             print("Key not found")
-    elif choice == '3':
+    elif choice == 'C':
         print("Thank you and Have a nice day:) ")
         break
