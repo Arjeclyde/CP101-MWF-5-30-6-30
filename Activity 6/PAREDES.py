a508f7b18f19a00569663b455b3e22eac26d15c9
unknown <86631463+Arjeclyde@users.noreply.github.com>
2025-01-04 08:11:19 -0800

    Create PAREDES.py

---
+++ Activity 6/PAREDES.py
@@ -0,0 +1,11 @@
+Name = input("What is your name: ")
+Year = input("What Year? and Course: ")
+print(f"So you are a {Name} {Year}")
+
+
+Message = "I was Born on {} {} {}".format("January", "9", "2006")
+print(Message)
+
+
+Word = input("BirthPlace: ")
+print("So you are born in %s" % Word)
